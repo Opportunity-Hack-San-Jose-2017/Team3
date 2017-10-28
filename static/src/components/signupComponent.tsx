d83aa7b12db904f6e095eaec096cba7342ec6a16
Joseph <jrichardson@jmstudios.net>
2017-10-28 16:52:45 -0700

    small form with interests displaying with checkbox but not removing from interests array

--- static/src/components/signupComponent.tsx
+++ static/src/components/signupComponent.tsx
@@ -1,22 +1,43 @@
 import * as React from 'react'
+import { interests } from '../models/interests'
 
-class SignupComponent extends React.Component<{}, {}> {
+interface VolenteerSignupState {
+    firstName: string,
+    lastName: string,
+    phone: string,
+    email: string,
+    volenteerInterests?: Array<string>,
+    password: string,
+    rePassword: string
+
+}
+
+class SignupComponent extends React.Component<{},VolenteerSignupState> {
     constructor(props) {
         super(props)
+        this.state = {
+            firstName: '',
+            lastName: '',
+            email: '',
+            phone: '',
+            password: '',
+            rePassword: '',
+            volenteerInterests: []
+        }
     }
 
     handleFirstName = (event) => {
         event.preventDefault()
         this.setState({
             ...this.state,
-            first_name: event.target.value
+            firstName: event.target.value
         })
     }
     handleLastName = (event) => {
         event.preventDefault()
         this.setState({
             ...this.state,
-            last_name: event.target.value
+            lastName: event.target.value
         })
     }
     handleEmail = (event) => {
@@ -37,26 +58,55 @@ class SignupComponent extends React.Component<{}, {}> {
         event.preventDefault()
         this.setState({
             ...this.state,
-            re_pass: event.target.value
+            rePassword: event.target.value
         })
     }
     handleRegister = (event) => {
         event.preventDefault()
         console.log(this.state)
     }
+    handleInterest = (event) => {
+        event.preventDefault()
+
+        var interest = event.target.value
+
+        var interests = this.state.volenteerInterests
+        var interestIndex = interests.indexOf(interest)
+        if (interestIndex == -1) {
+            interests.push(interest)
+        }
+        else {
+            interests.splice(interestIndex, 1)
+            console.log(interests)
+            console.log(interestIndex)
+        }
+        this.setState({
+            ...this.state,
+            volenteerInterests: interests
+        })
+    }
 
     render () {
         return (
             <div>
                 <div>
-                    <input type='text' placeholder='First Name' onChange={this.handleFirstName}/><br />
-                    <input type='text' placeholder='Last Name' onChange={this.handleLastName}/><br />
-                    <input type='email' placeholder='Email' onChange={this.handleEmail}/><br />
-                    <input type='password' placeholder='Passphrase' onChange={this.handlePassword}/><br />
-                    <input type='password' placeholder='Retype Passphrase' onChange={this.handleRePass}/><br />
+                    <label >First Name: </label><input type='text' placeholder='First Name' onChange={this.handleFirstName}/><br />
+                    <label>Last Name: </label><input type='text' placeholder='Last Name' onChange={this.handleLastName}/><br />
+                    <label>Email: </label><input type='email' placeholder='Email' onChange={this.handleEmail}/><br />
+                    <label>Password: </label><input type='password' placeholder='Passphrase' onChange={this.handlePassword}/><br />
+                    <label>Re Password: </label><input type='password' placeholder='Retype Passphrase' onChange={this.handleRePass}/><br />
                 </div>
                 <div>
                     Interests list to select from
+                    {
+                        interests.map( (interest, index) => {
+                            return (
+                            <div key={index}>
+                                <input type='checkbox' value={interest} onChange={this.handleInterest}/><span>{interest}</span>
+                            </div>
+                            )
+                        })
+                    }
                 </div>
                 <div>
                     Skill list to select from
---
+++ static/src/models/interests.ts
@@ -0,0 +1,16 @@
+var interests = [
+    'Grant Writing',
+    'Graphic Design/Web Design',
+    'Orphan Database Management',
+    'Web Content Management',
+    'Marketing',
+    'Corporate/Legal',
+    'Event Planning',
+    'Teaching',
+    'Public Speaking',
+    'Other'
+]
+
+export {
+    interests
+}
