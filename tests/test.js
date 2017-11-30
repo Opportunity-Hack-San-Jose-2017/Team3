6d7a0bbfdc5bc30b3689b267cf810a157afd8a74
Joseph <jrichardson@jmstudios.net>
2017-11-30 12:05:52 -0800

    useful test passing in props is working nicely

--- tests/test.js
+++ tests/test.js
@@ -3,25 +3,46 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from '../app/components/App'
-import { shallow, configure } from 'enzyme'
+import VolunteerDisplayComponent from '../app/components/admin/volunteerDisplay/VolunteerDisplayComponent'
+import { shallow, configure, mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-15'
 
 configure({ adapter: new Adapter() })
 
-describe('main js app component', function() {
+describe('VolunteerDisplay component tests', () => {
+    
+    it ('test VolunteerDisplay props', () => {
+        const testVolunteer = {
+            name: 'test name',
+            email: 'test@tests.com',
+            phone: '234345456',
+            country: 'United States',
+            region: 'California',
+            skills: ['testing'],
+            interests: ['Gardening']
+        }
+        const wrapper = shallow(<VolunteerDisplayComponent key={1} volunteerData={testVolunteer} />)
+        // const mountedWrapper = mount(<VolunteerDisplayComponent key={1} volunteerData={testVolunteer} />)
+        //console.log(mountedWrapper) for mount to work NEED JSDOM package since mount uses `document` and needs it defined within the test, mount is used for testing the react component life cycles if need to
+        expect(wrapper.instance().props.volunteerData).to.equal(testVolunteer)
+    })
+})
+
+describe('main js app component', () => {
     const defaultProps = {
         dispatch: () => {},
         manifest: { components: [] },
     }
 
-    beforeEach(function() {
-        console.log(App)
-    });
+    beforeEach( () => {
+        //console.log(App)
+    })
     
     it('should render without blowing up', () => {
         const wrapper = shallow(<App />)
-        console.log('first test')
-        expect(wrapper.length).to.eql(1);
+        console.log(wrapper)
+        console.log(Object.keys(wrapper))
+        expect(wrapper.length).to.eql(1)
     })
 
 
